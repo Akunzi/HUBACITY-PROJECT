3583b5fe6ab5dc969ae77d53e88f245a71280f8f
Akunna Anyamkpa <Akunna.Anyamkpa@stu.cu.edu.ng>
2019-02-22 09:27:57 -0800

    PART1

---
+++ HUBACITY/COMBINE/Home.js
@@ -0,0 +1,29 @@
+$(window).on('scroll', function() {
+    if($(window).scrollTop()){
+        $('nav').addclass('black');
+    }
+    else{
+        $('nav').removeClass('black'); 
+    }
+})  
+
+
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.container'); 
+
+left.addEventListener('mouseenter', () => {
+    container.classList.add('hover-left'); 
+}); 
+
+left.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-left');  
+}); 
+
+right.addEventListener('mouseenter', () => { 
+    container.classList.add('hover-right');  
+}); 
+
+right.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-right');   
+});
